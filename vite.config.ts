c6ab15fea50646aa2607d25af09be800ee84c94d
Guyutongxue <guyutongxue@163.com>
2024-02-23 22:03:43 +0800

    add highlight

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
+import remarkMath from "remark-math";
+import remarkGfm from "remark-gfm";
+import remarkFrontmatter from "remark-frontmatter";
+import rehypeKaTeX from "rehype-katex";
+import rehypeHighlight from "rehype-highlight";
+import { all } from "lowlight";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/CppEnvSetup/",
   plugins: [
     {
-      ...mdx(),
+      ...mdx({
+        remarkPlugins: [remarkGfm, remarkMath, remarkFrontmatter],
+        rehypePlugins: [rehypeKaTeX, [rehypeHighlight, { languages: all }]],
+      }),
       enforce: "pre",
     },
     react(),
